0c48545f1782a634d49b6c75b11ae6fdbd9813a8
Sanjay Soundarajan <sanjay.16@live.com>
2024-06-27 16:39:44 -0700

    👷 ci: update azure workflow file

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,13 +13,13 @@ const toggleMobileMenu = () => {
     class="relative mx-auto flex h-full min-h-screen w-full flex-col bg-slate-50"
     :class="{ 'debug-screens': devMode }"
   >
-    <div class="relative z-20 mx-auto w-full bg-slate-800 px-5 text-white">
+    <div class="relative z-20 mx-auto w-full px-5">
       <header class="mb-2 flex items-center justify-between py-4">
         <nav class="hidden items-center gap-4 lg:flex">
           <NuxtLink
             href="/"
-            class="mr-4 inline-flex items-center gap-2.5 text-xl font-bold"
             aria-label="logo"
+            class="mr-4 inline-flex items-center gap-2.5"
           >
             <img
               src="https://www.svgrepo.com/show/227168/log-wood.svg"
@@ -27,28 +27,21 @@ const toggleMobileMenu = () => {
               class="h-8 w-8"
             />
 
-            logwatch
-          </NuxtLink>
-
-          <NuxtLink
-            href="/applications"
-            class="font-medium text-white transition-all hover:text-slate-200"
-          >
-            Dashboard
-          </NuxtLink>
-
-          <NuxtLink
-            href="/applications"
-            class="font-medium text-white transition-all hover:text-slate-200"
-          >
-            Applications
+            <span class="text-xl font-bold"> logwatch </span>
           </NuxtLink>
         </nav>
 
         <nav class="hidden items-center gap-4 lg:flex">
           <NuxtLink
+            href="/applications"
+            class="font-medium transition-all hover:text-slate-700"
+          >
+            Applications
+          </NuxtLink>
+
+          <NuxtLink
             href="https://github.com/megasanjay/logwatch"
-            class="font-medium text-white transition-all hover:text-slate-200"
+            class="font-medium transition-all hover:text-slate-700"
           >
             <div class="flex items-center">
               GitHub
